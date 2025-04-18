290fed96002e8ac66544dacad97f2b0c22778e90
Declan0805 <Declanschroeder8@gmail.com>
2025-04-18 14:15:14 -0500

    Python

--- television.py
+++ television.py
@@ -24,3 +24,40 @@ class Television:
         #Checks if tv is on first
         if self.__status:
             self.__muted = not self.__muted
+    """
+    Channel and volume functions change the channel and volume integer variables
+    """
+    def channel_up(self) -> None:
+        #First checks if the TV is on, then checks if it is less than the max channel
+
+        if self.__status:
+            if self.__channel < Television.MAX_CHANNEL:
+                self.__channel += 1
+            else:
+                #If the channel is greater than the max_Channel it loops back down to the minimum channel
+                self.__channel = Television.MIN_CHANNEL
+    def channel_down(self) -> None:
+        if self.__status:
+            if self.__channel > Television.MIN_CHANNEL:
+                self.__channel -= 1
+            else:
+                #If the channel is less than the minimum value it reverts it back to the max channel value
+                self.__channel = Television.MAX_CHANNEL
+    def volume_up(self) -> None:
+        if self.__status:
+            if self.__muted:
+                #If tv is muted it unmutes the tv
+                self.__muted = False
+            if self.__volume < Television.MAX_VOLUME:
+                #If volume is less than max it increases the volume
+                self.__volume += 1
+
+    def volume_down(self) -> None:
+        if self.__status:
+            if self.__muted:
+                self.__muted = False
+            if self.__volume > Television.MIN_VOLUME:
+                #If volume is greater than min volume it decreases volume by 1
+                self.__volume -= 1
+    def __str__(self) -> str:
+        return f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}"
